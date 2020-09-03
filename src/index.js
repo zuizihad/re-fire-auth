787dfe27bf4b4012fa64f91b0318b3abffeef7b0
zuizihad <ahmedxihad@gmail.com>
2020-09-03 13:44:47 +0600

    firebase connect with react app

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
  
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
+import Firebase, { FirebaseContext } from './components/Firebase'
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+    </FirebaseContext.Provider>, document.getElementById('root'));
  
 serviceWorker.unregister();
