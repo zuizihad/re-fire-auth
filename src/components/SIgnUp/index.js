b3d6daacd10acf63ba828a8fc3e3918135e2591b
zuizihad <ahmedxihad@gmail.com>
2020-09-04 16:49:37 +0600

    signup functionality created with out form

--- src/components/SIgnUp/index.js
+++ src/components/SIgnUp/index.js
@@ -1,9 +1,49 @@
-import React from 'react';
- 
-const index = () => (
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../contants/routes'
+
+
+const SignUpPage = () => (
   <div>
     <h1>Sign Up</h1>
   </div>
 );
  
-export default index;
+const INITIAL_STATE = {
+  userName: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null
+
+}
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE }
+  }
+  onSubmit = event =>{
+
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  render(){
+    const { userName, email, passwordOne, passwordTwo, error } = this.state;
+    return(
+      <form onSubmit={this.onSubmit}>
+        
+      </form>
+    )
+  }
+}
+
+const SignUpLink = () => {
+  <p>Don't have an account? <Link to={ROUTES.SIGNUP}>Sign Up</Link></p>
+}
+
+export default SignUpPage
+
+export { SignUpLink, SignUpForm }
