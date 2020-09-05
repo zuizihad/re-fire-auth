8ab6689fe69ab022c035f966bb2bbca938f8ef00
zuizihad <ahmedxihad@gmail.com>
2020-09-05 15:29:25 +0600

    session handling and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navigation from './components/Navigation/index'
 import Landing from './components/Landing'
@@ -9,24 +9,45 @@ import Account from './components/Account'
 import Admin from './components/Admin'
 import PasswordForgetPage from './components/PasswordForget'
 import * as ROUTES from './components/contants/routes'
-const App = () => (
-  <div>
-  <Router>
-    
-      <Navigation/>
-        <hr/>
+import { withFirebase } from './components/Firebase'
+import { auth } from 'firebase';
 
-        <Route exact path={ROUTES.LANDING} component={Landing}/>
-        <Route path={ROUTES.HOME} component={Home}/>
-        <Route path={ROUTES.SIGNUP} component={SignUp} />
-        <Route path={ROUTES.SIGNIN} component={SignIn}/>
-        <Route path={ROUTES.ACCOUNT} component={Account}/>
-        <Route path={ROUTES.ADMIN} component={Admin}/>
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
-    
-  </Router>
-  </div>
-  
-);
- 
-export default App;
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      authUser: null
+    }
+  }
+
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(
+      authUser => {
+      authUser 
+      ? this.setState({ authUser })
+      : this.setState({ authUser: null })
+    })
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+  render(){
+    return(
+      <div>
+      <Router>
+        <Navigation authUser={ this.state.authUser }/>
+          <hr/>
+            <Route exact path={ROUTES.LANDING} component={Landing}/>
+            <Route path={ROUTES.HOME} component={Home}/>
+            <Route path={ROUTES.SIGNUP} component={SignUp} />
+            <Route path={ROUTES.SIGNIN} component={SignIn}/>
+            <Route path={ROUTES.ACCOUNT} component={Account}/>
+            <Route path={ROUTES.ADMIN} component={Admin}/>
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+      </Router>
+      </div>
+    )
+  }
+}
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,34 +2,46 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import * as ROUTES from '../contants/routes'
 import SignOutButton from '../SignOut'
+import { auth } from 'firebase'
 
-const Navigation = () => {
-    return (
-        <nav className=" material indigo darken-2 ">
-        <div className="nav-wrapper nav-extended navbar-fixed">
-            <ul className="right hide-on-med-and-down">
-                <li>
-                    <Link to={ROUTES.SIGNIN}>SIgn In</Link> 
-                </li>
-                <li>
-                    <Link to={ROUTES.LANDING}>Landing</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ACCOUNT}>Account</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ADMIN}>Admin</Link>
-                </li>
-                <li>
-                    <SignOutButton/>
-                </li>
-            </ul>
-        </div>
-        </nav>
-    )
-}
+const Navigation = ({ authUser }) => (
+    <div>{authUser ? <NavigationAuth/> : <NavigationNonAuth/>}</div>
+)
+
+const NavigationAuth = () => (
+    <nav className=" material indigo darken-2 ">
+    <div className="nav-wrapper nav-extended navbar-fixed">
+        <ul className="right hide-on-med-and-down">
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+                <SignOutButton/>
+            </li>
+        </ul>
+    </div>
+    </nav>
+)
+
+const NavigationNonAuth = () => (
+    <nav className=" material indigo darken-2 ">
+    <div className="nav-wrapper nav-extended navbar-fixed">
+        <ul className="right hide-on-med-and-down">
+            <li>
+                <Link to={ROUTES.SIGNIN}>Sign In</Link> 
+            </li>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>       
+        </ul>
+    </div>
+    </nav>
+)
 
 export default Navigation
