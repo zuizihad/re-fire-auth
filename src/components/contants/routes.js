131561e37b2047e44f7ad569b5c29cee447d6544
Zihadul Islam <ahmedxihad@gmail.com>
2020-10-07 12:33:05 +0600

    react advance routing

--- src/components/contants/routes.js
+++ src/components/contants/routes.js
@@ -5,4 +5,5 @@ export const HOME = '/home'
 export const ACCOUNT = '/account'
 export const ADMIN = '/admin'
 export const PASSWORD_FORGET = '/pw_forget'
+export const ADMIN_DETAILS = '/admin/:id';
 
